86fd0f3748ccdd910a2219667bb8a89960e2d290
Zi90 <edwin0322@gmail.com>
2024-10-22 17:52:47 +0900

    day02

---
+++ src/main/java/dao/BoardDao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.util.List;
+
+import domain.BoardVO;
+
+public interface BoardDao {
+
+	int insert(BoardVO bvo);
+
+	List<BoardVO> getList();
+
+	BoardVO getDetail(int bno);
+
+}
